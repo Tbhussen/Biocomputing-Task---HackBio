6dd141715b79ec84360466f50e81db6263b5214b
Tbhussen <tamim.b.hussein@gmail.com>
2025-08-30 15:45:04 +0300

    Create project2.sh

---
+++ project2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Stage 0 – Project 2: Installing Bioinformatics Software on the Terminal
+# Author: Tamim Hussein
+# HackBio Internship
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh 
+conda init
+conda --version
+conda create -n funtools python=3.10
+conda activate funtools
+sudo apt-get install figlet
+figlet Tamim
+conda config --add channels bioconda;conda config --add channels conda-forge
+conda install bwa
+conda install blast
+conda install samtools bedtools bcftools fastp multiqc
+conda install spades
